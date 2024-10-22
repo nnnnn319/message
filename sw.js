6ea43c5db2014ed0450dcdb390b884926379cfa9
nnnnn319 <993357345@qq.com>
2024-10-22 20:10:34 +0800

    service worker + 3

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
 let data = null
 self.addEventListener('message', (e) => {
     console.log('service recieve message', e.data)
+    
     data = e.data
+    self.clients.matchAll().then(function (clients) {
+        if (!clients || clients.length === 0) {
+            console.log('clients 0')
+            return;
+        }
+        clients.forEach(function (client) {
+            client.postMessage(data);
+        });
+    })
 })
 
 
