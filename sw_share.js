70611b3e4c34f107de4ee958e2abe591d183916a
nnnnn319 <993357345@qq.com>
2024-10-23 09:54:24 +0800

    share worker 2

--- sw_share.js
+++ sw_share.js
@@ -2,17 +2,16 @@ let data = null
 let port = null
 self.addEventListener('connect', (e) => {
     port = e.ports[0]
-    
+    port.addEventListener('message', (e) => {
+        if(e.data.get) {
+            // 
+            console.log('data get')
+            data && port.postMessage(data)
+        } else {
+            console.log('data post')
+            data = e.data
+        }
+    })
     port.start()
 })
 
-port.addEventListener('message', (e) => {
-    if(e.data.get) {
-        // 
-        console.log('data get')
-        data && port.postMessage(data)
-    } else {
-        console.log('data post')
-        data = e.data
-    }
-})
