7473a5f09ddf08cc6ff7b7913bd38ccf1f1edacb
nnnnn319 <993357345@qq.com>
2024-10-23 10:17:46 +0800

    share worker + service worker

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
 let data = null
 self.addEventListener('message', (e) => {
-    console.log('service recieve message', e.data)
+    if(e.get) {
+        console.log('service get  message')
+        self.clients.matchAll().then(function (clients) {
+            if (!clients || clients.length === 0) {
+                console.log('clients 0')
+                return;
+            }
+            clients.forEach(function (client) {
+                client.postMessage(data);
+            });
+        })
+    } else {
+        console.log('service set message')
+        data = e.data
+    }
+    // console.log('service recieve message', e.data)
 
-    self.clients.matchAll().then(function (clients) {
-        if (!clients || clients.length === 0) {
-            console.log('clients 0')
-            return;
-        }
-        clients.forEach(function (client) {
-            client.postMessage(data);
-        });
-    })
-    data = e.data
+   
 })
 
 
